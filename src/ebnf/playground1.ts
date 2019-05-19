32c726be8334987521886eaad0d54085ca439cc3
Wouter van Heeswijk <woutervh@gmail.com>
2019-05-19 21:52:06 +0200

    Re-order code

--- src/ebnf/playground1.ts
+++ src/ebnf/playground1.ts
@@ -56,239 +56,6 @@ interface BnfGrammar {
     [Key: string]: BnfProduction;
 }
 
-function createTerminal(character: string): Terminal {
-    return { type: 'terminal', character };
-}
-
-const letter: Choice = {
-    type: 'choice',
-    children: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('').map(createTerminal)
-};
-
-const digit: Choice = {
-    type: 'choice',
-    children: '0123456789'.split('').map(createTerminal)
-};
-
-const symbol: Choice = {
-    type: 'choice',
-    children: `[]{}()<>'"=|.,;`.split('').map(createTerminal)
-};
-
-const character: Choice = {
-    type: 'choice',
-    children: [
-        { type: 'reference', name: 'letter' },
-        { type: 'reference', name: 'digit' },
-        { type: 'reference', name: 'symbol' },
-        { type: 'terminal', character: '_' }
-    ]
-};
-
-const identifier: Sequence = {
-    type: 'sequence',
-    children: [
-        { type: 'reference', name: 'letter' },
-        {
-            type: 'repeat',
-            child: {
-                type: 'choice',
-                children: [
-                    { type: 'reference', name: 'letter' },
-                    { type: 'reference', name: 'digit' },
-                    { type: 'terminal', character: '_' }
-                ]
-            }
-        }
-    ]
-};
-
-const terminal: Choice = {
-    type: 'choice',
-    children: [
-        {
-            type: 'sequence',
-            children: [
-                { type: 'terminal', character: `'` },
-                { type: 'reference', name: 'character' },
-                { type: 'repeat', child: { type: 'reference', name: 'character' } },
-                { type: 'terminal', character: `'` }
-            ]
-        },
-        {
-            type: 'sequence',
-            children: [
-                { type: 'terminal', character: '"' },
-                { type: 'reference', name: 'character' },
-                { type: 'repeat', child: { type: 'reference', name: 'character' } },
-                { type: 'terminal', character: '"' }
-            ]
-        }
-    ]
-};
-
-const lhs: Reference = { type: 'reference', name: 'identifier' };
-
-const rhs: Choice = {
-    type: 'choice',
-    children: [
-        {
-            type: 'sequence',
-            children: [
-                { type: 'terminal', character: '[' },
-                { type: 'reference', name: 'rhs' },
-                { type: 'terminal', character: ']' }
-            ]
-        },
-        {
-            type: 'sequence',
-            children: [
-                { type: 'terminal', character: '{' },
-                { type: 'reference', name: 'rhs' },
-                { type: 'terminal', character: '}' }
-            ]
-        },
-        {
-            type: 'sequence',
-            children: [
-                { type: 'terminal', character: '(' },
-                { type: 'reference', name: 'rhs' },
-                { type: 'terminal', character: ')' }
-            ]
-        },
-        {
-            type: 'sequence',
-            children: [
-                { type: 'reference', name: 'rhs' },
-                { type: 'terminal', character: ',' },
-                { type: 'reference', name: 'rhs' }
-            ]
-        },
-        {
-            type: 'sequence',
-            children: [
-                { type: 'reference', name: 'rhs' },
-                { type: 'terminal', character: '|' },
-                { type: 'reference', name: 'rhs' }
-            ]
-        },
-        { type: 'reference', name: 'identifier' },
-        { type: 'reference', name: 'terminal' }
-    ]
-};
-
-const rule: Sequence = {
-    type: 'sequence',
-    children: [
-        { type: 'reference', name: 'lhs' },
-        { type: 'terminal', character: '=' },
-        { type: 'reference', name: 'rhs' },
-        { type: 'terminal', character: ';' }
-    ]
-};
-
-const rules: Repeat = {
-    type: 'repeat',
-    child: {
-        type: 'reference',
-        name: 'rule'
-    }
-};
-
-const grammar: EbnfGrammar = {
-    letter,
-    digit,
-    symbol,
-    character,
-    identifier,
-    terminal,
-    lhs,
-    rhs,
-    rule,
-    rules
-};
-
-const input = `E=E,'^',E|'-',E|E,('*'|'/'),E|E,('+'|'-'),E|ID|NUMBER;S=ID,'=',E,';';P={S}`;
-const characters = input.split('');
-let indent = '';
-
-function parseTerminal(terminal: Terminal, position: number) {
-    if (characters[position] === terminal.character) {
-        return 1;
-    } else {
-        return -1;
-    }
-}
-
-function parseChoice(grammar: EbnfGrammar, rule: Choice, position: number) {
-    for (const child of rule.children) {
-        const advance = parse(grammar, child, position);
-        if (advance >= 0) {
-            return advance;
-        }
-    }
-    return -1;
-}
-
-function parseReference(grammar: EbnfGrammar, rule: Reference, position: number) {
-    console.log(`${indent}${rule.name}`);
-    indent += '  ';
-    const result = parse(grammar, grammar[rule.name], position);
-    indent = indent.substr(2);
-    return result;
-}
-
-function parseRepeat(grammar: EbnfGrammar, rule: Repeat, position: number) {
-    let advance = 0;
-    while (true) {
-        const next = parse(grammar, rule.child, position + advance);
-        if (next >= 0) {
-            advance += next;
-        } else {
-            break;
-        }
-    }
-    return advance;
-}
-
-function parseSequence(grammar: EbnfGrammar, rule: Sequence, position: number) {
-    let advance = 0;
-    for (const child of rule.children) {
-        const next = parse(grammar, child, position + advance);
-        if (next >= 0) {
-            advance += next;
-        } else {
-            return -1;
-        }
-    }
-    return advance;
-}
-
-function parseOptional(grammar: EbnfGrammar, rule: Optional, position: number) {
-    const advance = parse(grammar, rule.child, position);
-    if (advance >= 0) {
-        return advance;
-    } else {
-        return 0;
-    }
-}
-
-function parseEmpty() {
-    return 0;
-}
-
-function parse(grammar: EbnfGrammar, rule: EbnfProduction, position: number): number {
-    switch (rule.type) {
-        case 'choice': return parseChoice(grammar, rule, position);
-        case 'empty': return parseEmpty();
-        case 'optional': return parseOptional(grammar, rule, position);
-        case 'reference': return parseReference(grammar, rule, position);
-        case 'repeat': return parseRepeat(grammar, rule, position);
-        case 'sequence': return parseSequence(grammar, rule, position);
-        case 'terminal': return parseTerminal(rule, position);
-    }
-}
-
 function toBnf(grammar: EbnfGrammar): BnfGrammar {
     const newGrammarRules: BnfGrammar = {};
     let newNonTerminalCounter = 0;
@@ -526,10 +293,243 @@ function removeLeftRecursion(grammar: BnfGrammar): BnfGrammar {
     return newGrammar;
 }
 
+function createTerminal(character: string): Terminal {
+    return { type: 'terminal', character };
+}
+
+const letter: Choice = {
+    type: 'choice',
+    children: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('').map(createTerminal)
+};
+
+const digit: Choice = {
+    type: 'choice',
+    children: '0123456789'.split('').map(createTerminal)
+};
+
+const symbol: Choice = {
+    type: 'choice',
+    children: `[]{}()<>'"=|.,;`.split('').map(createTerminal)
+};
+
+const character: Choice = {
+    type: 'choice',
+    children: [
+        { type: 'reference', name: 'letter' },
+        { type: 'reference', name: 'digit' },
+        { type: 'reference', name: 'symbol' },
+        { type: 'terminal', character: '_' }
+    ]
+};
+
+const identifier: Sequence = {
+    type: 'sequence',
+    children: [
+        { type: 'reference', name: 'letter' },
+        {
+            type: 'repeat',
+            child: {
+                type: 'choice',
+                children: [
+                    { type: 'reference', name: 'letter' },
+                    { type: 'reference', name: 'digit' },
+                    { type: 'terminal', character: '_' }
+                ]
+            }
+        }
+    ]
+};
+
+const terminal: Choice = {
+    type: 'choice',
+    children: [
+        {
+            type: 'sequence',
+            children: [
+                { type: 'terminal', character: `'` },
+                { type: 'reference', name: 'character' },
+                { type: 'repeat', child: { type: 'reference', name: 'character' } },
+                { type: 'terminal', character: `'` }
+            ]
+        },
+        {
+            type: 'sequence',
+            children: [
+                { type: 'terminal', character: '"' },
+                { type: 'reference', name: 'character' },
+                { type: 'repeat', child: { type: 'reference', name: 'character' } },
+                { type: 'terminal', character: '"' }
+            ]
+        }
+    ]
+};
+
+const lhs: Reference = { type: 'reference', name: 'identifier' };
+
+const rhs: Choice = {
+    type: 'choice',
+    children: [
+        {
+            type: 'sequence',
+            children: [
+                { type: 'terminal', character: '[' },
+                { type: 'reference', name: 'rhs' },
+                { type: 'terminal', character: ']' }
+            ]
+        },
+        {
+            type: 'sequence',
+            children: [
+                { type: 'terminal', character: '{' },
+                { type: 'reference', name: 'rhs' },
+                { type: 'terminal', character: '}' }
+            ]
+        },
+        {
+            type: 'sequence',
+            children: [
+                { type: 'terminal', character: '(' },
+                { type: 'reference', name: 'rhs' },
+                { type: 'terminal', character: ')' }
+            ]
+        },
+        {
+            type: 'sequence',
+            children: [
+                { type: 'reference', name: 'rhs' },
+                { type: 'terminal', character: ',' },
+                { type: 'reference', name: 'rhs' }
+            ]
+        },
+        {
+            type: 'sequence',
+            children: [
+                { type: 'reference', name: 'rhs' },
+                { type: 'terminal', character: '|' },
+                { type: 'reference', name: 'rhs' }
+            ]
+        },
+        { type: 'reference', name: 'identifier' },
+        { type: 'reference', name: 'terminal' }
+    ]
+};
+
+const rule: Sequence = {
+    type: 'sequence',
+    children: [
+        { type: 'reference', name: 'lhs' },
+        { type: 'terminal', character: '=' },
+        { type: 'reference', name: 'rhs' },
+        { type: 'terminal', character: ';' }
+    ]
+};
+
+const rules: Repeat = {
+    type: 'repeat',
+    child: {
+        type: 'reference',
+        name: 'rule'
+    }
+};
+
+const grammar: EbnfGrammar = {
+    letter,
+    digit,
+    symbol,
+    character,
+    identifier,
+    terminal,
+    lhs,
+    rhs,
+    rule,
+    rules
+};
+
+const input = `E=E,'^',E|'-',E|E,('*'|'/'),E|E,('+'|'-'),E|ID|NUMBER;S=ID,'=',E,';';P={S}`;
+const characters = input.split('');
+let indent = '';
+
+function parseTerminal(terminal: Terminal, position: number) {
+    if (characters[position] === terminal.character) {
+        return 1;
+    } else {
+        return -1;
+    }
+}
+
+function parseChoice(grammar: EbnfGrammar, rule: Choice, position: number) {
+    for (const child of rule.children) {
+        const advance = parse(grammar, child, position);
+        if (advance >= 0) {
+            return advance;
+        }
+    }
+    return -1;
+}
+
+function parseReference(grammar: EbnfGrammar, rule: Reference, position: number) {
+    console.log(`${indent}${rule.name}`);
+    indent += '  ';
+    const result = parse(grammar, grammar[rule.name], position);
+    indent = indent.substr(2);
+    return result;
+}
+
+function parseRepeat(grammar: EbnfGrammar, rule: Repeat, position: number) {
+    let advance = 0;
+    while (true) {
+        const next = parse(grammar, rule.child, position + advance);
+        if (next >= 0) {
+            advance += next;
+        } else {
+            break;
+        }
+    }
+    return advance;
+}
+
+function parseSequence(grammar: EbnfGrammar, rule: Sequence, position: number) {
+    let advance = 0;
+    for (const child of rule.children) {
+        const next = parse(grammar, child, position + advance);
+        if (next >= 0) {
+            advance += next;
+        } else {
+            return -1;
+        }
+    }
+    return advance;
+}
+
+function parseOptional(grammar: EbnfGrammar, rule: Optional, position: number) {
+    const advance = parse(grammar, rule.child, position);
+    if (advance >= 0) {
+        return advance;
+    } else {
+        return 0;
+    }
+}
+
+function parseEmpty() {
+    return 0;
+}
+
+function parse(grammar: EbnfGrammar, rule: EbnfProduction, position: number): number {
+    switch (rule.type) {
+        case 'choice': return parseChoice(grammar, rule, position);
+        case 'empty': return parseEmpty();
+        case 'optional': return parseOptional(grammar, rule, position);
+        case 'reference': return parseReference(grammar, rule, position);
+        case 'repeat': return parseRepeat(grammar, rule, position);
+        case 'sequence': return parseSequence(grammar, rule, position);
+        case 'terminal': return parseTerminal(rule, position);
+    }
+}
+
 printGrammar(grammar);
 console.log('-------------------------------------');
 printGrammar(toBnf(grammar));
 console.log('-------------------------------------');
 printGrammar(removeLeftRecursion(toBnf(grammar)));
 
-// console.log(parse(grammar, rules, 0));
+console.log(parse(removeLeftRecursion(toBnf(grammar)), rules, 0));
