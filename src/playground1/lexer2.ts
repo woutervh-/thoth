76b5b1d0de3d72a36a190d65fa5e72a7f76d5dbf
Wouter van Heeswijk <woutervh@gmail.com>
2022-03-08 17:04:33 +0100

    Seemingly working range splitter

--- src/playground1/lexer2.ts
+++ src/playground1/lexer2.ts
@@ -211,50 +211,32 @@ function toFSM<T>(term: Term<T>): [number, FSM<number, T>] {
 }
 
 function toNormalizedTransitions<S>(fsm: FSM<S, [number, number]>) {
-    const s0s = fsm.transitions.map(([s, i, t]): [0 | 1, number] => [0, i[0]]);
-    const s1s = fsm.transitions.map(([s, i, t]): [0 | 1, number] => [1, i[1]]);
-    const ss = [...s0s, ...s1s].sort((a, b) => a[1] - b[1]);
-
     const ranges = fsm.transitions.map(([s, i, t]) => i).sort((a, b) => a[0] - b[0]);
-
     console.log(ranges.map(([a, b]) => `${Buffer.from([a])}-${Buffer.from([b])}`));
 
+    const i0s = fsm.transitions.map(([s, i, t]): [number, number] => [1, i[0]]);
+    const i1s = fsm.transitions.map(([s, i, t]): [number, number] => [-1, i[1]]);
+    const is = [...i0s, ...i1s].sort((a, b) => a[1] - b[1]);
+
+    let open = 1;
+    let last = is[0][1];
     const answer: [number, number][] = [];
-    let open: [number, number][] = [[-1, -1], ranges[0]];
-    for (let i = 1; i < ranges.length; i++) {
-        const left = open[0];
-        const right = open[1];
-        const current = ranges[i];
-
-
-
-        if (current[0] === open[0]) {
-            if (current[1] > open[1]) {
-                answer.push([open[0], open[1]]);
-                open = [open[1] + 1, current[1]];
-            } else if (current[1] < open[1]) {
-                answer.push([current[0], current[1]]);
-                open = [current[1] + 1, open[1]];
+    for (let i = 1; i < is.length; i++) {
+        if (is[i][0] === 1) {
+            if (last < is[i][1]) {
+                if (open >= 1) {
+                    answer.push([last, is[i][1] - 1]);
+                }
+                last = is[i][1];
             }
-        } else if (current[0] > open[1]) {
-            answer.push([open[0], open[1]]);
-            open = [current[0], current[1]];
         } else {
-            if (current[1] > open[1]) {
-                answer.push([open[0], current[0] - 1]);
-                answer.push([current[0], open[1]]);
-                open = [open[1] + 1, current[1]];
-            } else if (current[1] < open[1]) {
-                answer.push([open[0], current[0] - 1]);
-                answer.push([current[0], current[1]]);
-                open = [current[1] + 1, open[1]];
-            } else {
-                answer.push([open[0], current[0] - 1]);
-                open = [current[0], current[1]];
+            if (last <= is[i][1]) {
+                answer.push([last, is[i][1]]);
             }
+            last = is[i][1] + 1;
         }
+        open += is[i][0];
     }
-    answer.push(...open);
 
     console.log(answer.map(([a, b]) => `${Buffer.from([a])}-${Buffer.from([b])}`));
 
